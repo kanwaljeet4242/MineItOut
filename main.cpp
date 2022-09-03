828d41474ed3f7fc099bec0862512675ef3a071f
hemansh <hemanshagnihotri27@gmail.com>
2022-09-03 12:17:05 +0530

    Revert "new makefile and used glfw insted of freeglut"
    This reverts commit 19d5921ff8ee92df92819803281367abe1843f23.

--- main.cpp
+++ main.cpp
@@ -1,37 +1,25 @@
 #include<GL/gl.h>
-#include<GLFW/glfw3.h>
+#include<GL/glut.h>
 #include "game.h"
 
 #define COLUMNS 40
 #define ROWS 40
 
 void display_callback();
-void reshape_callback(GLFWwindow* window,int w, int h);
+void reshape_callback(int w, int h);
 void init();
 
 int main(int argc, char **argv)
 {
-    GLFWwindow *window;
-    if(!glfwInit())
-		return -1;
-	window=glfwCreateWindow(500,500,"Snake",NULL,NULL);
-	if(!window)
-	{
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwMakeContextCurrent(window);
-	glfwSetFramebufferSizeCallback(window, reshape_callback);
+    
+    glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(500,500);
+    glutCreateWindow("Snake");
+    glutDisplayFunc(display_callback);
+	glutReshapeFunc(reshape_callback);
 	init();
-	while(!glfwWindowShouldClose(window))
-	{
-		glClear(GL_COLOR_BUFFER_BIT);
-		drawGrid();
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-	glfwTerminate();
+    glutMainLoop();
     return 0;
 }
 
@@ -39,13 +27,15 @@ int main(int argc, char **argv)
 void init(){
 	glClearColor(0.2,0.4,0.8,1);
 	initGrid(COLUMNS,ROWS);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0.0,COLUMNS,0.0,ROWS,-1.0,1.0);	
-	glMatrixMode(GL_MODELVIEW);
 }
 
-void reshape_callback(GLFWwindow* window,int w, int h){
+void display_callback(){
+	glClear(GL_COLOR_BUFFER_BIT);
+	drawGrid();
+    glutSwapBuffers();
+}
+
+void reshape_callback(int w, int h){
 	glViewport(0,0,(GLsizei)w,(GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
